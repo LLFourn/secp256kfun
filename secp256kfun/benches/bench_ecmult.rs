27401e5ea8d991dacc5f92d997c28dda9dde4989
LLFourn <lloyd.fourn@gmail.com>
2021-11-10 16:55:53 +1100

    Expand mult benchmarks

--- secp256kfun/benches/bench_ecmult.rs
+++ secp256kfun/benches/bench_ecmult.rs
@@ -5,9 +5,6 @@ use secp256kfun::{g, marker::*, Point, Scalar, G};
 fn scalar_mul_point(c: &mut Criterion) {
     let mut group = c.benchmark_group("ecmult");
 
-    group.sample_size(600);
-    group.measurement_time(std::time::Duration::from_secs(60));
-
     group.bench_function("scalar_mul_point:basepoint,secret", |b| {
         b.iter_batched(
             || Scalar::random(&mut rand::thread_rng()),
@@ -77,5 +74,67 @@ fn scalar_mul_point(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, scalar_mul_point);
+fn double_mul(c: &mut Criterion) {
+    let mut group = c.benchmark_group("double_mul");
+
+    group.bench_function("double_mul:normal,public", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Scalar::random(&mut rand::thread_rng()).mark::<Public>(),
+                    Scalar::random(&mut rand::thread_rng()).mark::<Public>(),
+                    Point::random(&mut rand::thread_rng()),
+                    Point::random(&mut rand::thread_rng()),
+                )
+            },
+            |(x, y, A, B)| g!(x * A + y * B),
+            BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("double_mul:normal,secret", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Scalar::random(&mut rand::thread_rng()),
+                    Scalar::random(&mut rand::thread_rng()),
+                    Point::random(&mut rand::thread_rng()),
+                    Point::random(&mut rand::thread_rng()),
+                )
+            },
+            |(x, y, A, B)| g!(x * A + y * B),
+            BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("double_mul:basepoint,public", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Scalar::random(&mut rand::thread_rng()).mark::<Public>(),
+                    Scalar::random(&mut rand::thread_rng()).mark::<Public>(),
+                    Point::random(&mut rand::thread_rng()),
+                )
+            },
+            |(x, y, B)| g!(x * G + y * B),
+            BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("double_mul:basepoint,secret", |b| {
+        b.iter_batched(
+            || {
+                (
+                    Scalar::random(&mut rand::thread_rng()),
+                    Scalar::random(&mut rand::thread_rng()),
+                    Point::random(&mut rand::thread_rng()),
+                )
+            },
+            |(x, y, B)| g!(x * G + y * B),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(benches, scalar_mul_point, double_mul);
 criterion_main!(benches);
