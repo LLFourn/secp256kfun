012f2d5c51399c94e627ce69e591b99c5606c554
h4sh3d <h4sh3d@protonmail.com>
2022-07-05 10:05:12 +0200

    fix: deterministic nonce in secp256k1-sys-0.5.0
    secp256k1-sys-0.5.0 introduced breaking changes in its nonce generation
    function when no auxiliary data is provided. Now the secret key is alway
    bitwise xored with a mask even no auxiliary is provided. The `zero_mask`
    used with no auxiliary is the result of:
    
        TaggedHash("BIP0340/aux", 0x0000..00)
    
    This commit implements the changes to stay compatible with secp-sys in
    tests.

--- schnorr_fun/tests/against_c_lib.rs
+++ schnorr_fun/tests/against_c_lib.rs
@@ -2,11 +2,53 @@
 use proptest::prelude::*;
 use schnorr_fun::{
     fun::{marker::*, proptest, secp256k1, Scalar},
-    nonce::Deterministic,
     Message, Schnorr,
 };
 use secp256k1::SECP256K1;
+use secp256kfun::{
+    digest::Digest,
+    hash::{HashAdd, Tagged},
+    nonce::{AddTag, NonceGen},
+};
 use sha2::Sha256;
+
+/// Compliance type for no aux BIP340 libsecp256k1 implementation.
+///
+/// This type is expected to be used in [`Schnorr`] context and receive a tag "BIP0340" to be
+/// compatible with BIP 340 no auxiliary data, i.e. aux is set to null 32-bytes array.
+#[derive(Clone, Debug, Default)]
+struct Bip340NoAux {
+    nonce_hash: Sha256,
+    aux_hash: Sha256,
+}
+
+impl NonceGen for Bip340NoAux {
+    type Hash = Sha256;
+    fn begin_derivation(&self, secret: &Scalar) -> Self::Hash {
+        let sec_bytes = secret.to_bytes();
+        let mut bytes = [0u8; 32];
+        let zero_mask = self.aux_hash.clone().add(&[0u8; 32]);
+        bytes.copy_from_slice(zero_mask.finalize().as_ref());
+
+        // bitwise xor the zero mask with secret
+        for (i, byte) in bytes.iter_mut().enumerate() {
+            *byte ^= sec_bytes[i]
+        }
+
+        self.nonce_hash.clone().add(bytes.as_ref())
+    }
+}
+
+impl AddTag for Bip340NoAux {
+    fn add_tag(self, tag: &str) -> Self {
+        Self {
+            nonce_hash: self
+                .nonce_hash
+                .tagged(&[tag.as_bytes(), b"/nonce"].concat()),
+            aux_hash: self.aux_hash.tagged(&[tag.as_bytes(), b"/aux"].concat()),
+        }
+    }
+}
 
 proptest! {
 
@@ -19,7 +61,7 @@ proptest! {
         let keypair = secp256k1::KeyPair::from_secret_key(&secp, key.clone().into());
         let secp_msg = secp256k1::Message::from_slice(&msg).unwrap();
         let sig = secp.sign_schnorr_no_aux_rand(&secp_msg, &keypair);
-        let schnorr = Schnorr::<Sha256,_>::new(Deterministic::<Sha256>::default());
+        let schnorr = Schnorr::<Sha256,_>::new(Bip340NoAux::default());
         let fun_keypair = schnorr.new_keypair(key);
         let fun_msg = Message::<Public>::raw(&msg);
         let fun_sig: secp256k1::schnorr::Signature = schnorr.sign(&fun_keypair, fun_msg).into();
