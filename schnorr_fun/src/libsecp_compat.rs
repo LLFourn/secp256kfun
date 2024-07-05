9626533678ae0de41b7e8d6a75c1ebe717076493
LLFourn <lloyd.fourn@gmail.com>
2024-07-05 13:51:04 +1000

    [compat] Add compatibility with secp256k1 v0.29.0

--- schnorr_fun/src/libsecp_compat.rs
+++ schnorr_fun/src/libsecp_compat.rs
@@ -31,3 +31,20 @@ mod v0_28 {
         }
     }
 }
+
+#[cfg(feature = "libsecp_compat_0_29")]
+mod v0_29 {
+    use secp256kfun::secp256k1_0_29::schnorr;
+
+    impl From<crate::Signature> for schnorr::Signature {
+        fn from(sig: crate::Signature) -> Self {
+            schnorr::Signature::from_slice(sig.to_bytes().as_ref()).unwrap()
+        }
+    }
+
+    impl From<schnorr::Signature> for crate::Signature {
+        fn from(sig: schnorr::Signature) -> Self {
+            crate::Signature::from_bytes(*sig.as_ref()).unwrap()
+        }
+    }
+}
