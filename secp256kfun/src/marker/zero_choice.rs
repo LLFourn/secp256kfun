253fd7dc5c56a5ef888fa034950bcdff3bb5ee4c
LLFourn <lloyd.fourn@gmail.com>
2025-01-28 11:25:19 +1100

    DecideZero::Out should be ZeroChoice
    Dunno how I missed this before. Also forgot to add the constraints on Out.

--- secp256kfun/src/marker/zero_choice.rs
+++ secp256kfun/src/marker/zero_choice.rs
@@ -13,16 +13,15 @@ pub struct NonZero;
 
 /// A marker trait implemented by [`Zero`] and [`NonZero`].
 ///
-/// Note it is rarely useful to define a function over any `Z: ZeroChoice`.
-/// This trait mostly just exists for consistency.
+/// It's sometimes useful to define a function over any `Z: ZeroChoice`.
 pub trait ZeroChoice:
     Default
     + Clone
     + PartialEq
     + Eq
     + Copy
-    + DecideZero<NonZero>
-    + DecideZero<Zero>
+    + DecideZero<NonZero, Out = Self>
+    + DecideZero<Zero, Out = Zero>
     + DecideZero<Self, Out = Self>
     + core::hash::Hash
     + Ord
@@ -59,7 +58,7 @@ impl ZeroChoice for NonZero {
 /// A trait to figure out whether the result of a multiplication should be [`Zero`] or [`NonZero`] at compile time.
 pub trait DecideZero<ZZ> {
     /// If both arguments are `NonZero` then `Out` will be `NonZero`, otherwise `Zero`.
-    type Out;
+    type Out: ZeroChoice;
 }
 
 impl<Z: ZeroChoice> DecideZero<Z> for Zero {
